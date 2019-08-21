4b7e27db12a5428918f52ad9e5be651bec8e1049
Steve Coffman <steve.coffman@ithaka.org>
2019-08-21 18:22:42 -0400

    j2m refactor

--- cmd/j2m/j2m.go
+++ cmd/j2m/j2m.go
@@ -10,6 +10,10 @@ import (
 	"strings"
 )
 
+type Jiration struct {
+	re   *regexp.Regexp
+	repl interface{}
+}
 
 func main() {
 
@@ -26,102 +30,126 @@ func main() {
 		str = string(stdInBytes)
 	}
 
-	// UnOrdered Lists
-	re1 := regexp.MustCompile(`(?m)^[ \t]*(\*+)\s+`)
-	first := ReplaceAllStringSubmatchFunc(re1, str, func(groups []string) string {
-		_, stars := groups[0], groups[1]
-		return strings.Repeat("  ", len(stars)-1) + "* "
-	})
-	// Ordered Lists
-	re2 := regexp.MustCompile(`(?m)^[ \t]*(#+)\s+`)
-	second := ReplaceAllStringSubmatchFunc(re2, first, func(groups []string) string {
-		_, nums := groups[0], groups[1]
-		return strings.Repeat("  ", len(nums)-1) + "1. "
-	})
-	// Headers 1-6
-	re3 := regexp.MustCompile(`(?m)^h([0-6])\.(.*)$`)
-	third := ReplaceAllStringSubmatchFunc(re3, second, func(groups []string) string {
-		_, level, content := groups[0], groups[1], groups[2]
-		i, _ := strconv.Atoi(level)
-		return strings.Repeat("#", i) + content
-	})
-	// Bold
-	re4 := regexp.MustCompile(`\*(\S.*)\*`)
-	fourth := re4.ReplaceAllString(third, "**$1**")
-	// Italic
-	re5 := regexp.MustCompile(`\_(\S.*)\_`)
-	fifth := re5.ReplaceAllString(fourth, "*$1*")
-	// Monospaced text
-	re6 := regexp.MustCompile(`\{\{([^}]+)\}\}`)
-	sixth := re6.ReplaceAllString(fifth, "`$1`")
-	// Citations (buggy)
-	re7 := regexp.MustCompile(`\?\?((?:.[^?]|[^?].)+)\?\?`)
-	seventh := re7.ReplaceAllString(sixth, "<cite>$1</cite>")
+	jirations := []Jiration{
+		{ // UnOrdered Lists
+			re: regexp.MustCompile(`(?m)^[ \t]*(\*+)\s+`),
+			repl: func(groups []string) string {
+				_, stars := groups[0], groups[1]
+				return strings.Repeat("  ", len(stars)-1) + "* "
+			},
+		},
+		{ //Ordered Lists
+			re: regexp.MustCompile(`(?m)^[ \t]*(#+)\s+`),
+			repl: func(groups []string) string {
+				_, nums := groups[0], groups[1]
+				return strings.Repeat("  ", len(nums)-1) + "1. "
+			},
+		},
+		{ //Headers 1-6
+			re: regexp.MustCompile(`(?m)^h([0-6])\.(.*)$`),
+			repl: func(groups []string) string {
+				_, level, content := groups[0], groups[1], groups[2]
+				i, _ := strconv.Atoi(level)
+				return strings.Repeat("#", i) + content
+			},
+		},
+		{ // Bold
+			re: regexp.MustCompile(`\*(\S.*)\*`),
+			repl :"**$1**",
+		},
+		{ // Italic
+			re: regexp.MustCompile(`\_(\S.*)\_`),
+			repl :"*$1*",
+		},
+		{ // Monospaced text
+			re: regexp.MustCompile(`\{\{([^}]+)\}\}`),
+			repl :"`$1`",
+		},
+		{ // Citations (buggy)
+			re: regexp.MustCompile(`\?\?((?:.[^?]|[^?].)+)\?\?`),
+			repl :"<cite>$1</cite>",
+		},
+		{ // Inserts
+			re: regexp.MustCompile(`\+([^+]*)\+`),
+			repl :"<ins>$1</ins>",
+		},
+		{ // Superscript
+			re: regexp.MustCompile(`\^([^^]*)\^`),
+			repl :"<sup>$1</sup>",
+		},
+		{ // Subscript
+			re: regexp.MustCompile(`~([^~]*)~`),
+			repl :"<sub>$1</sub>",
+		},
+		{ // Strikethrough
+			re: regexp.MustCompile(`(\s+)-(\S+.*?\S)-(\s+)`),
+			repl :"$1~~$2~~$3",
+		},
+		{ // Code Block
+			re: regexp.MustCompile(`\{code(:([a-z]+))?([:|]?(title|borderStyle|borderColor|borderWidth|bgColor|titleBGColor)=.+?)*\}`),
+			repl :"```$2",
+		},
+		{ // Code Block End
+			re: regexp.MustCompile(`{code}`),
+			repl :"```",
+		},
+		{ // Pre-formatted text
+			re: regexp.MustCompile(`{noformat}`),
+			repl :"```",
+		},
+		{ // Un-named Links
+			re: regexp.MustCompile(`\[([^|]+)\]`),
+			repl :"<$1>",
+		},
+		{ // Images
+			re: regexp.MustCompile(`!(.+)!`),
+			repl :"![]($1)",
+		},
+		{ // Named Links
+			re: regexp.MustCompile(`\[(.+?)\|(.+)\]`),
+			repl :"[$1]($2)",
+		},
+		{ // Single Paragraph Blockquote
+			re: regexp.MustCompile(`(?m)^bq\.\s+`),
+			repl :"> ",
+		},
+		{ // Remove color: unsupported in md
+			re: regexp.MustCompile(`(?m)\{color:[^}]+\}(.*)\{color\}`),
+			repl :"$1",
+		},
+		{ // panel into table
+			re: regexp.MustCompile(`(?m)\{panel:title=([^}]*)\}\n?(.*?)\n?\{panel\}`),
+			repl :"\n| $1 |\n| --- |\n| $2 |",
+		},
+		{ //table header
+			re: regexp.MustCompile(`(?m)^[ \t]*((?:\|\|.*?)+\|\|)[ \t]*$`),
+			repl: func(groups []string) string {
+				_, headers := groups[0], groups[1]
+				reBarred := regexp.MustCompile(`\|\|`)
 
-	// Inserts
-	re8 := regexp.MustCompile(`\+([^+]*)\+`)
-	eighth := re8.ReplaceAllString(seventh, "<ins>$1</ins>")
-	// Superscript
-	re9:= regexp.MustCompile(`\^([^^]*)\^`)
-	ninth := re9.ReplaceAllString(eighth, "<sup>$1</sup>")
+				singleBarred := reBarred.ReplaceAllString(headers, "|")
+				fillerRe := regexp.MustCompile(`\|[^|]+`)
+				return "\n" + singleBarred + "\n" + fillerRe.ReplaceAllString(singleBarred, "| --- ")
+			},
+		},
+		{ // remove leading-space of table headers and rows
+			re: regexp.MustCompile(`(?m)^[ \t]*\|`),
+			repl :"|",
+		},
+	}
 
-	// Subscript
-	re10:= regexp.MustCompile(`~([^~]*)~`)
-	tenth := re10.ReplaceAllString(ninth, "<sub>$1</sub>")
-	// Strikethrough
-	re11:= regexp.MustCompile(`(\s+)-(\S+.*?\S)-(\s+)`)
-	eleventh := re11.ReplaceAllString(tenth, "$1~~$2~~$3")
-	// Code Block
-	re12:= regexp.MustCompile(`\{code(:([a-z]+))?([:|]?(title|borderStyle|borderColor|borderWidth|bgColor|titleBGColor)=.+?)*\}`)
-	twelfth := re12.ReplaceAllString(eleventh, "```$2")
+	for _, jiration := range jirations {
+		switch v := jiration.repl.(type) {
+		case string:
+			str = jiration.re.ReplaceAllString(str, v)
+		case func([]string) string:
+			str = ReplaceAllStringSubmatchFunc(jiration.re, str,v)
+		default:
+			fmt.Printf("I don't know about type %T!\n", v)
+		}
+	}
 
-	re13:= regexp.MustCompile(`{code}`)
-	thirteenth := re13.ReplaceAllString(twelfth, "```")
-     // Pre-formatted text
-    re14:= regexp.MustCompile(`{noformat}`)
-	fourteenth := re14.ReplaceAllString(thirteenth, "```")
-
-	// Un-named Links
-	re15:= regexp.MustCompile(`\[([^|]+)\]`)
-	fifteenth := re15.ReplaceAllString(fourteenth, "<$1>")
-
-
-	// Images
-	re16:= regexp.MustCompile(`!(.+)!`)
-	sixteenth := re16.ReplaceAllString(fifteenth, "![]($1)")
-	// Named Links
-	re17:= regexp.MustCompile(`\[(.+?)\|(.+)\]`)
-	seventeenth := re17.ReplaceAllString(sixteenth, "[$1]($2)")
-
-	// Single Paragraph Blockquote
-	re18:= regexp.MustCompile(`(?m)^bq\.\s+`)
-	eighteenth := re18.ReplaceAllString(seventeenth, "> ")
-
-	// Remove color: unsupported in md
-	re19:= regexp.MustCompile(`(?m)\{color:[^}]+\}(.*)\{color\}`)
-	nineteenth := re19.ReplaceAllString(eighteenth, "$1")
-
-	// panel into table
-	re20:= regexp.MustCompile(`(?m)\{panel:title=([^}]*)\}\n?(.*?)\n?\{panel\}`)
-	twentieth := re20.ReplaceAllString(nineteenth, "\n| $1 |\n| --- |\n| $2 |")
-
-	// table header
-	re21:= regexp.MustCompile(`(?m)^[ \t]*((?:\|\|.*?)+\|\|)[ \t]*$`)
-	twentyfirst := ReplaceAllStringSubmatchFunc(re21, twentieth, func(groups []string) string {
-		_, headers := groups[0], groups[1]
-		reBarred:= regexp.MustCompile(`\|\|`)
-
-		singleBarred :=  reBarred.ReplaceAllString(headers, "|")
-		fillerRe := regexp.MustCompile(`\|[^|]+`)
-		return "\n" + singleBarred + "\n" + fillerRe.ReplaceAllString(singleBarred, "| --- ")
-	})
-
-	// remove leading-space of table headers and rows
-	re22:= regexp.MustCompile(`(?m)^[ \t]*\|`)
-	twentysecond := re22.ReplaceAllString(twentyfirst, "|")
-
-
-	fmt.Printf("%s\n", twentysecond)
+	fmt.Printf("%s\n", str)
 }
 
 // https://gist.github.com/elliotchance/d419395aa776d632d897
